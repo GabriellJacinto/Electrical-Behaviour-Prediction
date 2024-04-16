954d1233b459da02d365de1d16c6074891801390
Gabriel Lima Jacinto <gabriellimajacinto@gmail.com>
2024-04-16 07:33:54 -0300

    okey dokey

---
+++ FINFET/5nm/NOT/Data/hspice-sim0/asap5_tt.pm
@@ -0,0 +1,8 @@
+.include ./allvt_cgp44_210623a/nwfet/nmos_lvt_tt_hc_nwfet_asap5_210623a.pm
+.include ./allvt_cgp44_210623a/nwfet/nmos_rvt_tt_hc_nwfet_asap5_210623a.pm
+.include ./allvt_cgp44_210623a/nwfet/nmos_slvt_tt_hc_nwfet_asap5_210623a.pm
+.include ./allvt_cgp44_210623a/nwfet/nmos_sram_tt_hc_nwfet_asap5_210623a.pm
+.include ./allvt_cgp44_210623a/nwfet/pmos_lvt_tt_hc_nwfet_asap5_210623a.pm
+.include ./allvt_cgp44_210623a/nwfet/pmos_rvt_tt_hc_nwfet_asap5_210623a.pm
+.include ./allvt_cgp44_210623a/nwfet/pmos_slvt_tt_hc_nwfet_asap5_210623a.pm
+.include ./allvt_cgp44_210623a/nwfet/pmos_sram_tt_hc_nwfet_asap5_210623a.pm
